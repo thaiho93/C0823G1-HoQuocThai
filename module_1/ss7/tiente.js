25533c344ff591f3e3f254123450402bd1a01e24
thaiho93 <thaiho1993@gmail.com>
2023-09-13 19:42:10 +0700

    update bai tap git

--- module_1/ss7/tiente.js
+++ module_1/ss7/tiente.js
@@ -5,16 +5,16 @@ function ChangeMoney() {
     let Result;
 
 
-    if (FromC == "USD" && To == "VND") {
-        Result = "Result: " + (Amount * 23500) + "đ";
+    if (FromC == "EUR" && To == "VND") {
+        Result = "Result: " + (Amount * 26500) + "đ";
     }
-    else if (FromC == "VND"&& To =="USD"){
-        Result = "Result: "+ (Amount / 23500) + "$";
+    else if (FromC == "VND"&& To =="EUR"){
+        Result = "Result: "+ (Amount / 26500) + "€";
     }
     else if (FromC == "VND") {
         Result = "Result: " + Amount + "đ"
     } else {
-        Result = "Result: " + Amount + "$"
+        Result = "Result: " + Amount + "€"
     }
 
     document.getElementById("Result").innerHTML = Result;
